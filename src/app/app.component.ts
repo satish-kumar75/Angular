9f431e4b99bd192708a58e9a32365c633de5339a
satsihkumar75 <Satishk404456@gmail.com>
2024-06-18 23:16:34 +0530

    Initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-angular-app';
+  isDisabled: boolean = false;
+  isActive = true;
+
+  toggleActive() {
+    this.isActive = !this.isActive;
+    console.log('isActive:', this.isActive); 
+  }
 }
